1d0589ca1b80c0829fc441d9ba2c4f76c9b4c0b6
TPower63064 <tpower.63064@farnborough.ac.uk>
2017-11-06 17:05:28 +0000

    more added in answer service

--- src/Model/AnswerService.java
+++ src/Model/AnswerService.java
@@ -1,9 +1,13 @@
 package Model;
 
+import java.io.Console;
 import java.util.List;
 
 public class AnswerService {
+
     public static void selectAll(List<Answer> destination, DatabaseConnection database) {}
-    public static Answer selectById(int answerID DatabaseConnection database) {}
+    public static Console selectById(int answerID DatabaseConnection database) { }
+    public static void save(Console console, DatabaseConnection database) { }
+    public static void deleteById(int id, DatabaseConnection database) { }
 
 }
